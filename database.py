ba7523db20b25bfb83dcdbff84996375f3dab9dd
GC2702 <gauravchoudhary2702@gmail.com>
2025-07-28 23:32:41 +0530

    hkgdfjy

--- database.py
+++ database.py
@@ -1,13 +1,38 @@
 import sqlite3 as a
-a1=a.coonect("laptop.db")
+a1=a.connect("laptop.db")
 a2=a1.cursor()
-a2.execute("create table data(serialno varchar(100) primary key,name varcahr(100),dispinch varcahr(100),panel varcahr(100),cpu varcahr(100),seriesno varcahr(100),ram varcahr(100),upram varcahr(100),battery varcahr(100),weight varcahr(100),igpu varcahr(100) deafualt NULL,dgpu varcahr(100) deafult NULL,vram varcahr(100) );")
-""""
+#a2.execute("create table data(serialno varchar(100) primary key,name varcahr(100),dispinch varcahr(100),panel varcahr(100),cpu varcahr(100),seriesno varcahr(100),ram varcahr(100),upram varcahr(100),battery varcahr(100),weight varcahr(100),igpu varcahr(100) default NULL,dgpu varcahr(100) default NULL,vram varcahr(100) );")
+
+a2.execute("alter table data add booklkey bool")
+"""
 a2.execute()
 a2.execute()
 a2.execute()
 a2.execute()
-a2.execute()
-""""
-a2.coomit()
+"""
+"""
+def abc(x):
+    x0=[]
+    
+    for i in range(x):
+        x1=int(input("enter the serial number:"))
+        x2=input("enter the name:")
+        x3=input("enter the display inch:")
+        x4=input("enter the panel:")
+        x5=input("enter the cpu:")
+        x6=input("enter the seies number:")
+        x7=input("enter the ram:")
+        x8=input("enter the upgradable ram:")
+        x9=input("enter the battery:")
+        x10=input("enter the weight:")
+        x11=input("enter the igpu:")
+        x12=input("enter the dgpu:")
+        x13=input("enter the vram:")
+        x14=input("enter the backlit keyboard:")
+        XX=[x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14]
+        a2.executemany("insert into data values()")
+        
+
+"""
+#a1.commit()
 #a1.close()
